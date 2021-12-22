2381d3d25a31333841128a64c47ec0bfe9691377
Anish Jiben <anishjiben@gmail.com>
2021-12-22 16:21:17 +0530

    test

--- app.js
+++ app.js
@@ -67,32 +67,30 @@ function load3dModles(){
 function loadImages(){
 	const urlParams = new URLSearchParams(window.location.search)
 	let modelId = urlParams.get("model")
-	const markerDiv = document.createElement("canvas");
+	const markerDiv = document.createElement("a-image");
 	markerDiv.setAttribute("rotation", "0 0 0" );
 	markerDiv.setAttribute("position", "0 0 0" );
-	markerDiv.setAttribute("class", "example");
-	// if( modelId == "1"){
-	// 	markerDiv.setAttribute("src", "models/Image/Osaka.gif");
-	// 	markerDiv.setAttribute("scale", "1 1 1");
-	// }
-	// else if( modelId == "3"){
-	// 	markerDiv.setAttribute("src", "models/Image/butterflies.gif");
-	// 	markerDiv.setAttribute("scale", "1 1 1");
-	// }
-	// else if( modelId == "2"){
-	// 	markerDiv.setAttribute("src", "models/Image/love.png");
-	// 	markerDiv.setAttribute("scale", "1 1 1");
-	// }
-	// else if( modelId == "4"){
-	// 	markerDiv.setAttribute("src", "models/Image/4.jpg");
-	// 	markerDiv.setAttribute("scale", "1 1 1");
-	// } else {
-	// 	markerDiv.setAttribute("src", "models/Image/5.jpg");
-	// 	markerDiv.setAttribute("scale", "1 1 1");
-	// }
+	if( modelId == "1"){
+		markerDiv.setAttribute("src", "models/Image/Osaka.gif");
+		markerDiv.setAttribute("scale", "1 1 1");
+	}
+	else if( modelId == "3"){
+		markerDiv.setAttribute("src", "models/Image/butterflies.gif");
+		markerDiv.setAttribute("scale", "1 1 1");
+	}
+	else if( modelId == "2"){
+		markerDiv.setAttribute("src", "models/Image/love.png");
+		markerDiv.setAttribute("scale", "1 1 1");
+	}
+	else if( modelId == "4"){
+		markerDiv.setAttribute("src", "models/Image/4.jpg");
+		markerDiv.setAttribute("scale", "1 1 1");
+	} else {
+		markerDiv.setAttribute("src", "models/Image/5.jpg");
+		markerDiv.setAttribute("scale", "1 1 1");
+	}
 	var element = document.getElementById("#modelEntity");
 	element.appendChild(markerDiv);	
-	gifler('https://anishjiben.github.io/webar-demo-main/models/Image/butterflies.gif').animate('.example')
 }
 
 
